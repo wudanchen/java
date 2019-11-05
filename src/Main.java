c26277432a6d4efca78f7010e2d05f8dee604f7c
wudancheng <wudancheng@linuxprobe.com>
2019-11-05 11:11:10 -0500

    [config][wudancheng][0]add repo
    [what]add repo
    [why]none
    [how]none

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package bin;
+
+public class Main{
+	public static void main(String[] args){
+		Person person=new Person();
+		person.name="世界人民大团结万岁";
+		person.say(person.name);
+		System.out.println(person.name);
+	}
+}
